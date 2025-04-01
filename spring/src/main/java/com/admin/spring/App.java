a36fc239ac0e88601e2450739cafe1721e3f125c
Abhinav Kumar Prajapati <itsverywierd@gmail.com>
2025-04-01 14:40:03 +0530

    spring loading and implementation

--- spring/src/main/java/com/admin/spring/App.java
+++ spring/src/main/java/com/admin/spring/App.java
@@ -1,13 +1,12 @@
 package com.admin.spring;
+import org.springframework.context.*;
+import org.springframework.context.support.*;
 
-/**
- * Hello world!
- *
- */
 public class App 
 {
     public static void main( String[] args )
     {
+    	ApplicationContext context = new ClassPathXmlApplicationContext("spring.xml");
     	Vehicle c = new Car();
     	c.drive();
     	Vehicle b = new Bike();
