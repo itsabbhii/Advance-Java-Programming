b221c36cebbc86a0a198daa8b372d17b5ff33836
Abhinav Kumar Prajapati <itsverywierd@gmail.com>
2025-04-01 12:28:56 +0530

    methods

--- spring/src/main/java/com/admin/spring/Bike.java
+++ spring/src/main/java/com/admin/spring/Bike.java
@@ -2,4 +2,7 @@ package com.admin.spring;
 
 public class Bike {
 
+	public void drive() {
+		System.out.println("Bike Running \nZooooooooooooom!");
+	}
 }
--- spring/src/main/java/com/admin/spring/Car.java
+++ spring/src/main/java/com/admin/spring/Car.java
@@ -1,5 +1,7 @@
 package com.admin.spring;
 
 public class Car {
-
+	public void drive() {
+		System.out.println("Car Running \nBrooooooooooom!!");
+	}
 }
