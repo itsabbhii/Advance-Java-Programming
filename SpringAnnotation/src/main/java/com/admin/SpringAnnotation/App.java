855be5214debe68f43ea77063ac515e99b9fb3f3
Abhinav Kumar Prajapati <itsverywierd@gmail.com>
2025-04-02 15:06:14 +0530

    Configuration using AppConfig

--- SpringAnnotation/src/main/java/com/admin/SpringAnnotation/App.java
+++ SpringAnnotation/src/main/java/com/admin/SpringAnnotation/App.java
@@ -1,13 +1,21 @@
 package com.admin.SpringAnnotation;
 
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.annotation.AnnotationConfigApplicationContext;
+
 /**
  * Hello world!
  *
  */
+
+
+
 public class App 
 {
     public static void main( String[] args )
     {
-        System.out.println( "Hello World!" );
+    	ApplicationContext factory = new AnnotationConfigApplicationContext(AppConfig.class);
+    	Samsung s25 = factory.getBean(Samsung.class);
+    	s25.config();
     }
 }
---
+++ SpringAnnotation/src/main/java/com/admin/SpringAnnotation/Samsung.java
@@ -0,0 +1,11 @@
+package com.admin.SpringAnnotation;
+
+
+public class Samsung {
+	public void config() {
+		System.out.println("S25 Ultra");
+		System.out.println("16 GB RAM || 256 GB UFS 3.2 Storage");
+		System.out.println("50 + 50 + 50 MP Camera");
+		System.out.println("120x Zoom");
+	}
+}
