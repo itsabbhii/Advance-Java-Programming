6a3bdb68f2c0ca6260dfd60456d4aeaf17f79fc0
Abhinav Kumar Prajapati <itsverywierd@gmail.com>
2025-04-04 12:20:27 +0530

    inserting rows

--- JDBC/src/main/java/com/admin/JDBC/App.java
+++ JDBC/src/main/java/com/admin/JDBC/App.java
@@ -1,5 +1,9 @@
 package com.admin.JDBC;
 
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.support.ClassPathXmlApplicationContext;
+import org.springframework.jdbc.core.JdbcTemplate;
+
 /**
  * Hello world!
  *
@@ -8,6 +12,13 @@ public class App
 {
     public static void main( String[] args )
     {
-        System.out.println( "Hello World!" );
+        ApplicationContext context = new ClassPathXmlApplicationContext("com/admin/JDBC/config.xml");
+        JdbcTemplate template = context.getBean("jdbcTemplate", JdbcTemplate.class);
+        
+        String query = "insert into student (id, name, city) values (?, ?, ?)";
+        
+        int result = template.update(query, 4, "Adarsh", "Delhi");
+        
+        System.out.println( result + " records inserted!!!" );
     }
 }
--- JDBC/src/main/java/com/admin/JDBC/Student.java
+++ JDBC/src/main/java/com/admin/JDBC/Student.java
@@ -1,5 +1,47 @@
 package com.admin.JDBC;
 
 public class Student {
+	private String name;
+	private String city;
+	private int id;
 
+	@Override
+	public String toString() {
+		return "Student [name=" + name + ", city=" + city + ", id=" + id + "]";
+	}
+	
+	public String getName() {
+		return name;
+	}
+	
+	public Student() {
+		super();
+	}
+	
+	public Student(String name, String city, int id) {
+		super();
+		this.name = name;
+		this.city = city;
+		this.id = id;
+	}
+	
+	public void setName(String name) {
+		this.name = name;
+	}
+	
+	public String getCity() {
+		return city;
+	}
+	
+	public void setCity(String city) {
+		this.city = city;
+	}
+	
+	public int getId() {
+		return id;
+	}
+	
+	public void setId(int id) {
+		this.id = id;
+	}
 }
