4a618f8c1fd7c41f5bfe8ca4d824cea85da57d70
Abhinav Kumar Prajapati <itsverywierd@gmail.com>
2025-04-07 11:49:37 +0530

    created add which uses @RequestParam to get parameters and Add service class

--- SpringDemoMVC/src/main/java/com/admin/SpringDemoMVC/AddController.java
+++ SpringDemoMVC/src/main/java/com/admin/SpringDemoMVC/AddController.java
@@ -5,6 +5,7 @@ import javax.servlet.http.HttpServletResponse;
 
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.servlet.ModelAndView;
 
 @Controller
@@ -24,15 +25,21 @@ public class AddController {
 //		return "display.jsp";
 //	}
 	
-	public ModelAndView add(HttpServletRequest req, HttpServletResponse res) {
-		int i = Integer.parseInt(req.getParameter("t1"));
-		int j = Integer.parseInt(req.getParameter("t2"));
-		
-		int k = i+j;
+//	public ModelAndView add(HttpServletRequest req, HttpServletResponse res) {
+//		int i = Integer.parseInt(req.getParameter("t1"));
+//		int j = Integer.parseInt(req.getParameter("t2"));
+//		
+//		int k = i+j;
+//		
+//		ModelAndView mv = new ModelAndView();
+//		mv.setViewName("display.jsp");
+//		mv.addObject("result", k);
+//		return mv;
+//	}
+	
+	public ModelAndView add(@RequestParam("t1") int i, @RequestParam("t2") int j) {//
 		
 		ModelAndView mv = new ModelAndView();
-		mv.setViewName("display.jsp");
-		mv.addObject("result", k);
 		return mv;
 	}
 }
