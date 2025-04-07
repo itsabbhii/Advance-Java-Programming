ea028f30e3712e196a6ebd1b257fa4fb6a8625dc
Abhinav Kumar Prajapati <itsverywierd@gmail.com>
2025-04-07 11:50:00 +0530

    sending k value to display.jsp

--- SpringDemoMVC/src/main/java/com/admin/SpringDemoMVC/AddController.java
+++ SpringDemoMVC/src/main/java/com/admin/SpringDemoMVC/AddController.java
@@ -8,6 +8,8 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.servlet.ModelAndView;
 
+import com.admin.service.AddService;
+
 @Controller
 public class AddController {
 	
@@ -37,9 +39,13 @@ public class AddController {
 //		return mv;
 //	}
 	
-	public ModelAndView add(@RequestParam("t1") int i, @RequestParam("t2") int j) {//
+	public ModelAndView add(@RequestParam("t1") int i, @RequestParam("t2") int j) {//gets parameter from request i.e. index.jsp
+		AddService as = new AddService();
+		int k = as.add(i, j);
 		
 		ModelAndView mv = new ModelAndView();
+		mv.setViewName("display.jsp");
+		mv.addObject("result", k);
 		return mv;
 	}
 }
