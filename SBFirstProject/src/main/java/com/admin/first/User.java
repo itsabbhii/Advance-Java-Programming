11f51f5c4a92f01465ed75cf40a5a3a8b4ef8b9d
Abhinav Kumar Prajapati <itsverywierd@gmail.com>
2025-04-07 14:37:49 +0530

    added getters and setter for user class

--- SBFirstProject/src/main/java/com/admin/first/User.java
+++ SBFirstProject/src/main/java/com/admin/first/User.java
@@ -1,5 +1,34 @@
 package com.admin.first;
 
 public class User {
-
+	private String uname;
+	private String tech;
+	private int uid;
+	
+	@Override
+	public String toString() {
+		return "in string";
+	}
+	public String getUname() {
+		return uname;
+	}
+	public void setUname(String uname) {
+		this.uname = uname;
+	}
+	public String getTech() {
+		return tech;
+	}
+	public void setTech(String tech) {
+		this.tech = tech;
+	}
+	public int getUid() {
+		return uid;
+	}
+	public void setUid(int uid) {
+		this.uid = uid;
+	}
+	public void show() {
+		// TODO Auto-generated method stub
+		System.out.println("In show");
+	}
 }
