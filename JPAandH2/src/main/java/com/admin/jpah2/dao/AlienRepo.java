e6740d9aede22d2ad8d48f4046f9c47cc193b42f
Abhinav Kumar Prajapati <itsverywierd@gmail.com>
2025-04-08 15:35:59 +0530

    done

--- JPAandH2/src/main/java/com/admin/jpah2/dao/AlienRepo.java
+++ JPAandH2/src/main/java/com/admin/jpah2/dao/AlienRepo.java
@@ -2,14 +2,23 @@ package com.admin.jpah2.dao;
 
 import java.util.List;
 
+import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.CrudRepository;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.ResponseBody;
 
 import com.admin.jpah2.model.Alien;
 
-public interface AlienRepo extends CrudRepository<Alien, Integer>{
-	List<Alien> findByAlang(String alang);
-	List<Alien> findByAidGreaterThan(int aid);
-	@Query("from Alien where alang=?1 order by aname")
-	List<Alien> findByAlangSorted(String alang);
+//public interface AlienRepo extends CrudRepository<Alien, Integer>{
+//	List<Alien> findByAlang(String alang);
+//	List<Alien> findByAidGreaterThan(int aid);
+//	@Query("from Alien where alang=?1 order by aname")
+//	List<Alien> findByAlangSorted(String alang);
+//}
+
+public interface AlienRepo extends JpaRepository<Alien, Integer>{
+	
+	
 }
