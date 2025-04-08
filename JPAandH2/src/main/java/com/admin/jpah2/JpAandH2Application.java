7d2939793d2302a806332e37c91d037a1e98a6fe
Abhinav Kumar Prajapati <itsverywierd@gmail.com>
2025-04-08 11:50:39 +0530

    spring boot and database project

---
+++ JPAandH2/src/main/java/com/admin/jpah2/JpAandH2Application.java
@@ -0,0 +1,13 @@
+package com.admin.jpah2;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JpAandH2Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(JpAandH2Application.class, args);
+	}
+
+}
---
+++ JPAandH2/src/test/java/com/admin/jpah2/JpAandH2ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.admin.jpah2;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JpAandH2ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
