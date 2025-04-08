a06e08dce0e971ba198674af248b54c08d1888dd
Abhinav Kumar Prajapati <itsverywierd@gmail.com>
2025-04-08 14:10:36 +0530

    dao

---
+++ JPAandH2/src/main/java/com/admin/jpah2/dao/AlienRepo.java
@@ -0,0 +1,10 @@
+package com.admin.jpah2.dao;
+
+import org.springframework.data.repository.CrudRepository;
+
+import com.admin.jpah2.model.Alien;
+
+public interface AlienRepo extends CrudRepository<Alien, Integer>{
+	
+
+}
