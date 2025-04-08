eabac85e3963f8cdcb544d7cea8a76d0abeacc12
Abhinav Kumar Prajapati <itsverywierd@gmail.com>
2025-04-08 14:10:26 +0530

    sql file

---
+++ JPAandH2/src/main/resources/data.sql
@@ -0,0 +1,4 @@
+insert into alien(aid, aname, alang) values(101, 'Abhinav', 'English');
+insert into alien(aid, aname, alang) values(102, 'Abhishek', 'Hindi');
+insert into alien(aid, aname, alang) values(103, 'Adarsh', 'Japanese');
+insert into alien(aid, aname, alang) values(104, 'Rahul', 'French');
