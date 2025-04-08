91723d4b27a77664b91fe9e87e187c4c0539cf70
Abhinav Kumar Prajapati <itsverywierd@gmail.com>
2025-04-08 14:10:44 +0530

    done

--- JPAandH2/src/main/java/com/admin/jpah2/controller/AlienController.java
+++ JPAandH2/src/main/java/com/admin/jpah2/controller/AlienController.java
@@ -4,10 +4,22 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 
+import com.admin.jpah2.dao.AlienRepo;
+import com.admin.jpah2.model.Alien;
+
 @Controller
 public class AlienController {
+	@Autowired
+	AlienRepo repo;
+	
 	@RequestMapping("/")
 	public String home() {
+		return "home";
+	}
+	
+	@RequestMapping("/addAlien")
+	public String addAlien(Alien alien) {
+		repo.save(alien);
 		return "home";
 	}
 }
